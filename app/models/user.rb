9d7f57770688b2e8dd2922860d1e3cdedd2c59cb
patrickwhardy <patrickwhardy@gmail.com>
2016-05-23 21:39:36 -0600

    test omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+
+  def self.from_omniauth(auth_info)
+    where(uid: auth_info[:uid]).first_or_create do |user|
+      user.uid          = auth_info.uid
+      user.first_name   = auth_info.extra.raw_info.firstname
+      user.last_name    = auth_info.extra.raw_info.lastname
+      user.avatar       = auth_info.extra.raw_info.profile
+      user.oauth_token  = auth_info.credentials.token
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523220150) do
+ActiveRecord::Schema.define(version: 20160524013221) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",  null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160523220150) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "uid"
+    t.string   "avatar"
   end
 
 end
